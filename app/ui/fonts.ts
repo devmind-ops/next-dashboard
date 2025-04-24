1cac3ebf6604ab75a50f5de384a8d054a0ee5a58
devmind-ops <hamzabhatti9922@gmail.com>
2025-04-24 13:06:25 +0500

    Font error deployment fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,12 @@
 import { Inter, Lusitana } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
 
-export const lusitana = Lusitana({ weight: "400" });
+// Configure the Inter font with Latin subset
+export const inter = Inter({
+  subsets: ['latin'],
+});
+
+// Configure the Lusitana font with weight 400
+export const lusitana = Lusitana({
+  weight: ['400'], // '400' should be inside an array
+  subsets: ['latin'], // It’s good practice to include subsets for better performance
+});
